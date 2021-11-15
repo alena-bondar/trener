a94881779b97b6bd551e5a91c14c0e38249a80a5
Andrij Nalyvayko <andrij.nalyvayko@gmail.com>
2021-11-15 16:46:42 +0200

    Created Wrapper

---
+++ client/src/components/helpers/SelectLanguage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {
+  setSelectedLanguage: any;
+};
+
+export const SelectLanguage: React.FC<Props> = ({ setSelectedLanguage }) => {
+  return <div></div>;
+};
