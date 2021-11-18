3f33ae237713d637e649643e857c84e6d462655d
Andrij Nalyvayko <andrij.nalyvayko@gmail.com>
2021-11-18 09:48:17 +0200

    add-application-wrapper (#6)
    * After review

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Header } from './components/header/Header';
+import { Footer } from './components/footer/Footer';
+import './styles/main.scss';
 
 const App = () => {
   return (
     <div className="App">
-      <h1>HoHoHo</h1>
+      <Header />
+      <Footer />
     </div>
   );
 };
---
+++ client/src/components/header/SelectCity.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {
+  setChoicedCity: any;
+};
+
+export const SelectCity: React.FC<Props> = ({ setChoicedCity }) => {
+  return <div></div>;
+};
