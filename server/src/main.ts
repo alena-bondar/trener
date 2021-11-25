2e5ace1cef36972346e61c6e623dac02ab261295
alena <bonda5229@gmail.com>
2021-11-25 22:11:18 +0200

    implemented authentication with jwt

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  app.use(cookieParser());
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Trainers')
--- server/src/trainers/trainers.module.ts
+++ server/src/trainers/trainers.module.ts
@@ -3,10 +3,19 @@ import { PostService } from './post.service';
 import { PostController } from './post.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostSchema } from './schemas/post.schema';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     MongooseModule.forFeature([{ name: 'Trainer', schema: PostSchema }]),
+    JwtModule.register({
+      secret: `${process.env.SECRET_KEY}`,
+      signOptions: { expiresIn: '1d' },
+    }),
   ],
   controllers: [PostController],
   providers: [PostService],
