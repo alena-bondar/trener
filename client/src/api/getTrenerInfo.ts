136f5bc321fc3951da3da141a46125b7ee09c990
Andrij Nalyvayko <andrij.nalyvayko@gmail.com>
2021-12-22 12:29:01 +0200

    Fixed

--- client/src/api/getTrenerInfo.ts
+++ client/src/api/getTrenerInfo.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 import { BASE_URL } from "services/BASE_URL";
-// import Treners from "../types/treners";
 
-export const getTrenerInfo = async (id = ""): Promise<any> => {
+export const getTrenerInfo = async (id = "") => {
   const result = await axios.get(BASE_URL + "/trainers/" + id);
-  // .then((res) => console.log(res));
 
   return result;
 };
